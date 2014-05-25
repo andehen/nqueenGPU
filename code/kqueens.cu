ca5a6048a03ba007c11ca3164c3bbe1533dd8f12
Anders Hennum <hennumjr@gmail.com>
2014-05-25 12:52:56 +0200

    Fixed some indention, added comments

--- code/kqueens.cu
+++ code/kqueens.cu
@@ -8,15 +8,19 @@
 
 using namespace std;
 
-__device__ float generate(curandState* globalState, int ind2 ) 
+__device__ float generate(curandState* globalState, int ind2) 
+// Function to generate random number in thread
 {
-    int ind = threadIdx.x;
-    curandState localState = globalState[ind];
-    float rnd = curand_uniform( &localState );
-    globalState[ind] = localState;
-    return rnd;
+	int ind = threadIdx.x;
+	curandState localState = globalState[ind];
+	float rnd = curand_uniform( &localState );
+	globalState[ind] = localState;
+	return rnd;
 }
-__device__ int diagonalsOK(int q,int i, int S[]){
+
+__device__ int checkDiagonals(int q,int i, int S[])
+// Returns 1 if no queen in diagonal, else 0
+{
 	int j = 1;
 	for (j; j<=i; j++){
 		if (S[i-j] == q-j | S[i-j] == q+j){
@@ -26,7 +30,9 @@ __device__ int diagonalsOK(int q,int i, int S[]){
 	return 1;
 }
 
-__device__ int sum(int row[], int len){
+__device__ int sum(int row[], int len)
+// Returns sum of an array 
+{
 	int i = 0;
 	int s = 0;
 	for (i; i<len; i++){
@@ -35,21 +41,21 @@ __device__ int sum(int row[], int len){
 	return s;
 }
 
-__global__ void setup_kernel ( curandState * state, unsigned long seed )
+__global__ void setup_kernel (curandState * state, unsigned long seed)
 {
-    int id = threadIdx.x;
-    curand_init ( seed, id, 0, &state[id] );
+	int id = threadIdx.x;
+	curand_init ( seed, id, 0, &state[id] );
 }
 
 __global__ void kernel(int* solution, curandState* globalState)
 {
 
 	// Initialize varaibles
-	int S[k];
-	int D[k];
-	int N[k][k];
+	int S[k]; 				// Holds current solution
+	int D[k];				// Rows where queens is placed
+	int N[k][k];			// Positions tried at column i
 	
-	int i = 0;
+	int i = 0;				
 	int j = 0;
 
 	int q; 
@@ -68,15 +74,15 @@ __global__ void kernel(int* solution, curandState* globalState)
 	
 	while (S[k-1] == -1){
 	
-        q = (generate(globalState, i) * k);
+		q = (generate(globalState, i) * k);	// Generate random number
 		
-		if (D[q] == 0 & N[i][q] == 0){ // Row clear and not tried before 
+		if (D[q] == 0 & N[i][q] == 0){ 		// Row clear and not tried before 
 			N[i][q] = 1;
-			if (diagonalsOK(q,i,S)==1){
-				S[i] = q;
+			if (checkDiagonals(q,i,S)==1){	// If no attacking queens in diagonal
+				S[i] = q;				  	// it can proceed
 				D[q] = 1;
 				i++;
-				if (i==k){ // Finished!
+				if (i==k){ 				  	// Finished!
 					break;
 				}
 			}
@@ -84,14 +90,14 @@ __global__ void kernel(int* solution, curandState* globalState)
 		if (sum(N[i],k) + sum(D,k) == k){
 			D[S[i-1]] = 0;
 			S[i-1] = -1;
-			// Set N[i] = 0;
 			j = 0;
-			for (j;j<k;j++){
+			for (j;j<k;j++){				// Reset N
 				N[i][j] = 0;
 			}		
-			i--; // Backtrack
+			i--; 							// Backtrack
 		}
 	}
+	// For now, just print solution for each thread for debugging
 	printf("Sol from block %d, thread %d: ", blockIdx, threadIdx);
 	for (int l=0;l<k;l++){
 		printf("%d ", S[l]);
@@ -107,22 +113,25 @@ int main()
 	size_t used = total - avail;
 	cout << "Device memory used: " << used << endl;
 
-    curandState* devStates;
-    cudaMalloc ( &devStates, k*sizeof( curandState ) );
+	curandState* devStates;
+	cudaMalloc ( &devStates, k*sizeof( curandState ) );
 
-    // setup seeds
-    setup_kernel <<< 1, k >>> ( devStates,unsigned(time(NULL)) );
-   
+	// Initialze seeds
+	setup_kernel <<< 10, 2 >>> ( devStates,unsigned(time(NULL)) );
+
 	int solution2[k];
 	int* solution3;
 
 	cudaMalloc((void**) &solution3, sizeof(int)*k);
 	 
-	kernel<<<2,1>>> (solution3, devStates);
+	kernel<<<10,2>>> (solution3, devStates);
 	cudaMemcpy(solution2, solution3, sizeof(int)*k, cudaMemcpyDeviceToHost);
 	
+	// Free memory
 	cudaFree(devStates);
 	cudaFree(solution3);
-    cudaDeviceReset();
-    return 0;
+	
+	cudaDeviceReset(); // Tried to fix memory leakage
+	
+	return 0;
 }
