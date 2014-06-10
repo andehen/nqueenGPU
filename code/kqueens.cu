a7ebf389c2b2be4c022c6c2988578aee37cb277a
Anders Hennum <hennumjr@gmail.com>
2014-06-10 09:26:53 +0200

    Removed unused code and fixed comments

--- code/kqueens.cu
+++ code/kqueens.cu
@@ -12,16 +12,6 @@
 
 using namespace std;
 
-//__device__ float generate(curandState* globalState, int ind)
-//// Function to generate random number in thread
-//{
-//	//int ind = (blockIdx.x+1)*threadIdx.x;
-//	curandState localState = globalState[ind];
-//	float rnd = curand_uniform( &localState );
-//	globalState[ind] = localState;
-//	return rnd;
-//}
-
 __device__ int checkDiagonals(int q,int i, int* S)
 // Returns 1 if no queen in diagonal, else 0
 {
@@ -32,15 +22,6 @@ __device__ int checkDiagonals(int q,int i, int* S)
 			return 0;
 		} 		
 	}	
-	return 1;
-}
-
-__device__ int checkSolution(int S[]){
-	for (int i=0;i<k;i++){
-		if (S[i]==-1){
-			return 0;
-		}
-	}
 	return 1;
 }
 
@@ -56,19 +37,19 @@ __device__ int sum(int row[], int len)
 }
 
 __global__ void setup_kernel (curandState * state, unsigned long seed)
+// Create states to generate random numbers
 {
 	int id = blockIdx.x*NUM_BLOCKS + threadIdx.x;
 	curand_init ( seed, id, 0, &state[id] );
 }
 
 __global__ void kernel(int* S, curandState* globalState)
+// Kernel to solve puzzle
 {
-	//__shared__ int S_shared[NUM_BLOCKS*NUM_THREADS*k];
+	// Index for thread to store solution
 	int I = blockIdx.x*NUM_THREADS*k + threadIdx.x*k;
 	int ind = blockIdx.x*NUM_BLOCKS + threadIdx.x;
-	// Initialize varaibles
-	//int S[k]; 				// Holds current solution
-	int D[k];				// Rows where queens is placed
+	int D[k];				// Rows where queens is placed. 1 = row taken
 	int N[k][k];				// Positions tried at column i
 	
 	int i = 0;				
@@ -76,7 +57,7 @@ __global__ void kernel(int* S, curandState* globalState)
 
 	int q; 
 	
-	// Set to start values
+	// Initialize variables
 	for (i; i<k; i++){
 		S[I+i] = -1;
 		D[i] = 0;
@@ -86,101 +67,85 @@ __global__ void kernel(int* S, curandState* globalState)
 		j = 0;
 	}
 
+	// Set start column and iter counter
 	i = 0;
-
 	int iter = 0;
 
-	// Get local state
+	// Get local state to generate numbers 
 	curandState localState = globalState[ind];
 	
 	while (iter < MAX_ITER){
 	
-		//q = (generate(globalState, ind) * k);	// Generate random number
+		// Generate random number
 		q = curand_uniform( &localState ) * k;
 		
 		if (D[q] == 0 & N[i][q] == 0){ 		// Row clear and not tried before 
-			N[i][q] = 1;
+			N[i][q] = 1;				// Set position as tried
 			if (checkDiagonals(q,i,S)==1){	// If no attacking queens in diagonal
-				S[I+i] = q;			// it can proceed
-				D[q] = 1;
-				i++;
+				S[I+i] = q;			// Add queen to solution
+				D[q] = 1;			// Set row as taken
+				i++;				// Increment interation counter
 				if (i==k){			// Finished!
 					break;
 				}
 			}
 		}
-		if (sum(N[i],k) + sum(D,k) == k){
-			D[S[I+i-1]] = 0;
-			S[I+i-1] = -1;
-			j = 0;
-			for (j;j<k;j++){		// Reset N
+		if (sum(N[i],k) + sum(D,k) == k){ 		// All positions tried
+			D[S[I+i-1]] = 0;					// Free domain
+			S[I+i-1] = -1;						// Remove queen from solution
+			j = 0;	
+			for (j;j<k;j++){		// Reset positions tried for column
 				N[i][j] = 0;
 			}		
-			i--;				// Backtrack
+			i--;				// Backtrack to prevoius column
 		}
 		iter++;
 	}
-	// For now, just print solution for each thread for debugging
-//	if (checkSolution(S)==1){
-//		printf("Sol from block %d, thread %d: ", blockIdx, threadIdx);
-//		for (int l=0;l<k;l++){
-//			printf("%d ", S[l]);
-//		}
-//		printf("\n");
-//	}
-
-	//for (int p=0;p<k;p++){
-	//	solution[I+p] = S[p];
-	//}
 }
 
 int main() 
 {
-//	size_t avail;
-//	size_t total;
-//	cudaMemGetInfo( &avail, &total );
-//	size_t used = total - avail;
-//	cout << "Device memory used: " << used << endl;
-
+	// Initialize states variable and allocate memory
 	curandState* devStates;
 	cudaMalloc ( &devStates, k*sizeof( curandState ) );
 
 	// Initialze seeds
 	setup_kernel <<< NUM_BLOCKS, NUM_THREADS>>> ( devStates,unsigned(time(NULL)) );
 
+	// Initialize array to store solution
 	int solution_host[k*NUM_BLOCKS*NUM_THREADS];
 	int* solution_dev;
 
+	// Allocate memory on device
 	cudaMalloc((void**) &solution_dev, sizeof(int)*k*NUM_BLOCKS*NUM_THREADS);
 
+	// Start clock
 	clock_t begin = clock();
+	// Launch kernel on device
 	kernel<<<NUM_BLOCKS,NUM_THREADS>>> (solution_dev, devStates);
+	// Copy solution from device to host
 	cudaMemcpy(solution_host, solution_dev, sizeof(int)*k*NUM_BLOCKS*NUM_THREADS, cudaMemcpyDeviceToHost);
+	// End clock
 	clock_t end = clock();
 	
 	double elapsed_sec = double(end - begin)/(CLOCKS_PER_SEC/1000);
 
+	// Print time used
 	cout << elapsed_sec << endl;
 
+	// Count solutions found (not -1 in last position)
 	int solution_count = 0;
 	for (int l=0;l<NUM_BLOCKS*NUM_THREADS;l++){
 		if (solution_host[l*k+k-1]!=-1){
-//			for (int p=0;p<k;p++){
-//				printf("%d ", solution_host[l*k+p]);
-//			}
-			//printf("\n");
 			solution_count++;
 		}
 	}
+	// Print solutions found
 	cout << solution_count << endl;
 
-	// Free memory
+	// Free memory on device
 	cudaFree(devStates);
 	cudaFree(solution_dev);
-	
-	cudaDeviceReset(); // Tried to fix memory leakage
 
 	return 0;
-
-
 }
